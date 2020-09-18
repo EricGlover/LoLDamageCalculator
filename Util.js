4a430dce8f368b335313911301945d60eb2549d4
Eric Glover <etgt28@mail.missouri.edu>
2020-09-17 19:26:47 -0500

    champion names

--- Util.js
+++ Util.js
@@ -1,5 +1,28 @@
 class Util {
+    async static loadFullChampionDetails() {
+        const championData = [];
+        const basePath = "./data/champions";
+        const dir = await fs.promises.opendir(basePath);
+        for await (const dirent of dir) {
+            if(dirent.isFile()) {
+                console.log(`reading ${dirent.name}`);
+                let data = fs.readFileSync(basePath + `/${dirent.name}`);
+                let obj = JSON.parse(data);
+                championData.push(Object.values(obj.data)[0]);
+            }
+        }
+        let championMap = new Map();
+        championData.map(data => {
+            let champion = Champion.makeFromObj(data);
+            championMap.set(champion.name, champion);
+        })
+        return championMap;
+    }
     static toNumberArr(str) {
         return str.split("/").map(str => Number.parseInt(str.trim()));
+    }
+
+    async static getChampionNames() {
+        return (await loadFullChampionDetails()).map(c => c.name);
     }
 }
--- test.js
+++ test.js
@@ -1,3 +1,30 @@
 const str = "50 / 60 / 70 / 80 / 90";
 const numbers = str.split("/").map(str => Number.parseInt(str.trim()));
+const fs = require('fs');
 console.log(numbers);
+class Util {
+     static async loadFullChampionDetails() {
+        const championData = [];
+        const basePath = "./data/champions";
+        const dir = await fs.promises.opendir(basePath);
+        for await (const dirent of dir) {
+            if(dirent.isFile()) {
+                console.log(`reading ${dirent.name}`);
+                let data = fs.readFileSync(basePath + `/${dirent.name}`);
+                let obj = JSON.parse(data);
+                championData.push(Object.values(obj.data)[0]);
+            }
+        }
+        return championData.map(c => c.name);
+    }
+    static toNumberArr(str) {
+        return str.split("/").map(str => Number.parseInt(str.trim()));
+    }
+
+    static async  getChampionNames() {
+        return Util.loadFullChampionDetails();
+    }
+}
+Util.getChampionNames().then(names => {
+    fs.writeFileSync('./data/championNames.json', JSON.stringify(names, null, 2));
+});
