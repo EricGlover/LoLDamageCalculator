cbe1cfbccad2a64141142ba24c53286185916235
Eric Glover <etgt28@mail.missouri.edu>
2020-09-19 22:20:12 -0500

    deleting cruft files

--- backend/DataScraper/Formatter.js
+++ backend/DataScraper/Formatter.js
@@ -3,11 +3,9 @@ class Formatter {
      * @param {string} str
      */
     stripTags(str) {
-        let clone = str;
         let reg = /(<([^>]+)>)/ig;
         return str.replace(reg, '');
     }
-
 }
 
 module.exports = Formatter;
